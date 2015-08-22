3883a5ab2149bdb5aa556da1186104b2d106c62d
Øyvind Raddum Berg <elacin@gmail.com>
2015-08-22 04:13:34 +0200

    added Optional.map

--- core/src/main/scala/japgolly/scalajs/react/vdom/Optional.scala
+++ core/src/main/scala/japgolly/scalajs/react/vdom/Optional.scala
@@ -3,6 +3,7 @@ package japgolly.scalajs.react.vdom
 import scala.scalajs.js.UndefOr
 
 trait Optional[T[_]] {
+  def map[A, B](t: T[A])(f: A => B): T[B]
   def foreach[A](t: T[A])(f: A => Unit): Unit
   def fold[A, B](t: T[A], b: => B)(f: A => B): B
   def isEmpty[A](t: T[A]): Boolean
@@ -10,12 +11,14 @@ trait Optional[T[_]] {
 
 object Optional {
   implicit val optionInstance: Optional[Option] = new Optional[Option] {
+    @inline final override def map[A, B] (t: Option[A])(f: (A) => B): Option[B] = t map f
     @inline final override def foreach[A](t: Option[A])(f: (A) => Unit): Unit = t foreach f
     @inline final override def fold[A, B](t: Option[A], b: => B)(f: A => B): B = t.fold(b)(f)
     @inline final override def isEmpty[A](t: Option[A]): Boolean = t.isEmpty
   }
 
   implicit val jsUndefOrInstance: Optional[UndefOr] = new Optional[UndefOr] {
+    @inline final override def map[A, B] (t: UndefOr[A])(f: (A) => B): UndefOr[B] = t map f
     @inline final override def foreach[A](t: UndefOr[A])(f: (A) => Unit): Unit = t foreach f
     @inline final override def fold[A, B](t: UndefOr[A], b: => B)(f: A => B): B = t.fold(b)(f)
     @inline final override def isEmpty[A](t: UndefOr[A]): Boolean = t.isEmpty
