1e4f6a41b00574579dbd7410a067d3eef37cfccd
Øyvind Raddum Berg <elacin@gmail.com>
2015-08-26 03:25:31 +0200

    static object

--- gh-pages/src/main/scala/ghpages/examples/PropsUpdateExample.scala
+++ gh-pages/src/main/scala/ghpages/examples/PropsUpdateExample.scala
@@ -1,8 +1,8 @@
 package ghpages.examples
 
 import ghpages.examples.util.SideBySide
-import japgolly.scalajs.react.extra.Reusability
-import japgolly.scalajs.react.{Callback, BackendScope, ReactComponentB}
+import japgolly.scalajs.react.extra._
+import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.prefix_<^._
 
 object PropsUpdateExample {
@@ -10,31 +10,50 @@ object PropsUpdateExample {
   def content = SideBySide.Content("", "", main())
 
   lazy val main = addIntro(Outer, identity)
+
+  case class InnerProps(
+    printer:     Int ~=> Callback,
+    onIncrement: Callback, 
+    onRefresh:   Callback, 
+    value:       Int
+  )
   
-  case class InnerProps(onIncrement: Callback, onRefresh: Callback, i: Int)
   case class InnerBackend(p: InnerProps, $: BackendScope[Nothing, Unit]){
     println(s"Initialized InnerBackend($p, ${$})")
-    val incrementBtn = <.button("increment " + p.i.toString, ^.onClick --> p.onIncrement)
-    val refreshBtn   = <.button("refresh", ^.onClick --> p.onRefresh)
+
+    val incrementBtn = <.button(
+      "increment " + p.value.toString,
+      ^.onClick --> (p.onIncrement >> p.printer(p.value))
+    )
+
+    val refreshBtn = <.button(
+      "refresh",
+      ^.onClick --> p.onRefresh
+    )
   }
 
-  implicit val r0 = Reusability.by((p: InnerProps) ⇒ p.i)
+  implicit val r0 = Reusability.by((p: InnerProps) ⇒ p.value)
 
   val Inner = ReactComponentB[InnerProps]("Inner")
     .backend(InnerBackend)
     .render($ ⇒ <.div($.backend.incrementBtn, $.backend.refreshBtn))
     .configure(Reusability.shouldComponentUpdate)
     .build
-
+  
   case class OuterBackend($: BackendScope[Nothing, Int]){
     val increment = $.modState(1 +)
     val refresh   = $.modState(0 +)
   }
 
+  object OuterStatic{
+    val print: Int ~=> Callback = 
+      ReusableFn(i ⇒ Callback(println(i)))
+  }
+  
   val Outer = ReactComponentB[Unit]("Outer")
     .initialState(0)
     .backendNoProps(OuterBackend)
-    .render($ => Inner(InnerProps($.backend.increment, $.backend.refresh, $.state)))
+    .render($ => Inner(InnerProps(OuterStatic.print, $.backend.increment, $.backend.refresh, $.state)))
     .buildU
 
 }
